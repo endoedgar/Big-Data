1b847781c69e1618f9d9455c7f51fd10cc48aa9b
Edgar de Jesus Endo Junior <endoedgarjunior@gmail.com>
2019-09-30 20:36:47 -0500

    Using streams instead of loops and FileScanner

--- Assigment W1D1/src/Main.java
+++ Assigment W1D1/src/Main.java
@@ -1,38 +1,35 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 	public static List<KeyValuePair<String, Integer>> splitFileIntoListStrings(String filepath) {
 		List<KeyValuePair<String, Integer>> result = new ArrayList<KeyValuePair<String, Integer>>();
-		try (Scanner scanner = new Scanner(new File(filepath))) {
-		    while (scanner.hasNext()) {
-		    	String line = scanner.nextLine();
-		    	Pattern p = Pattern.compile("([a-zA-Z0-9]+)");
-		    	Matcher m = p.matcher(line);
-		    	while(m.find()) {
-		    		if(!m.group(1).matches(".*\\d.*"))
-		    			result.add(new KeyValuePair<String, Integer>(m.group(1).toLowerCase(),1));
-		    	}
-		    }
-		} catch (FileNotFoundException fnfe) {
+		try (Stream<String> stream = Files.lines(Paths.get(filepath))) {
+			Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
+		    result = stream
+		    		.flatMap(line -> Arrays.asList(line.split("[ -\\._]")).stream())
+		    		.filter(pattern.asPredicate())
+		    		.map(o -> new KeyValuePair<String, Integer>(o.toLowerCase(),1))
+		    		.sorted((i1, i2) -> i1.getT().compareTo(i2.getT()))
+		    		.collect(Collectors.toList());
+		} catch (IOException fnfe) {
 		    fnfe.printStackTrace();
 		}
-
 		return result;
 	}
 	public static List<KeyValuePair<String, Integer>> extractListOfWordsFromFile(String filepath) {
 		List<KeyValuePair<String, Integer>> strings = splitFileIntoListStrings(filepath);
-		strings.sort((i1, i2) -> i1.getT().compareTo(i2.getT()));
 		return strings;
 	}
 	public static void main(String[] args) {
 		List<KeyValuePair<String, Integer>> strings = extractListOfWordsFromFile("test.txt");
 		strings.stream().forEach(System.out::println);
-
 	}
 }
