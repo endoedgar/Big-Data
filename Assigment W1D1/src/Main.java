4ff29864518e6e94bcad86c4c7d41df5b2bc7451
Edgar de Jesus Endo Junior <endoedgarjunior@gmail.com>
2019-09-30 22:04:28 -0500

    Even more optimizations

--- Assigment W1D1/src/Main.java
+++ Assigment W1D1/src/Main.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -12,10 +11,9 @@ public class Main {
 	public static List<KeyValuePair<String, Integer>> splitFileIntoListStrings(String filepath) {
 		List<KeyValuePair<String, Integer>> result = new ArrayList<KeyValuePair<String, Integer>>();
 		try (Stream<String> stream = Files.lines(Paths.get(filepath))) {
-			Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
 		    result = stream
-		    		.flatMap(line -> Arrays.asList(line.split("[ -\\._]")).stream())
-		    		.filter(pattern.asPredicate())
+		    		.flatMap(line -> Pattern.compile("[ -\\._]").splitAsStream(line))
+		    		.filter(Pattern.compile("^[a-zA-Z]+$").asPredicate())
 		    		.map(o -> new KeyValuePair<String, Integer>(o.toLowerCase(),1))
 		    		.sorted((i1, i2) -> i1.getT().compareTo(i2.getT()))
 		    		.collect(Collectors.toList());
